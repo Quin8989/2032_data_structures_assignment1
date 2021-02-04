80ea1897c63871672c097e460dbed473c54b1a07
Q <Q@DESKTOP-60KPC6M.home>
2021-02-04 18:13:31 -0500

    First Commit

---
+++ 2032assignment1/src/SkipList.java
@@ -0,0 +1,141 @@
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+import java.util.TreeMap;
+
+public class SkipList<T> implements List<T> {
+
+	@Override
+	public boolean add(T arg0) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void add(int arg0, T arg1) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public T get(int arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void clear() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return null;
+
+	}
+	
+	/* Below are any unused methods that aren't needed for the implementation,
+	 * but are necessary for using the List interface.
+	 */
+
+	@Override
+	public boolean addAll(Collection<? extends T> arg0) {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public boolean contains(Object arg0) {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public boolean containsAll(Collection<?> arg0) {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public int indexOf(Object arg0) {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public boolean isEmpty() {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public Iterator<T> iterator() {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public boolean addAll(int arg0, Collection<? extends T> arg1) {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public int lastIndexOf(Object arg0) {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public ListIterator<T> listIterator() {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public ListIterator<T> listIterator(int arg0) {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public boolean remove(Object arg0) {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public T remove(int arg0) {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public boolean removeAll(Collection<?> arg0) {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public boolean retainAll(Collection<?> arg0) {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public T set(int arg0, T arg1) {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public List<T> subList(int arg0, int arg1) {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public Object[] toArray() {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public <T> T[] toArray(T[] arg0) {
+		throw new UnsupportedOperationException();
+	}
+
+}
---
+++ 2032assignment1/src/assignment1_test.java
@@ -0,0 +1,12 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class assignment1_test {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
