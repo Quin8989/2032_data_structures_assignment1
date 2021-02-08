29a8357a603fb4c597c9050f6ac7cbee55c7deb7
Q <Q@DESKTOP-60KPC6M.home>
2021-02-08 14:04:22 -0500

    added assignNodeHeight method

--- 2032assignment1/src/SkipList.java
+++ 2032assignment1/src/SkipList.java
@@ -6,53 +6,65 @@ import java.util.ListIterator;
 import java.util.TreeMap;
 
 public class SkipList<T> implements List<T> {
-	private TreeMap <Integer, T> map;
+	private TreeMap<Integer, T> map;
 	private int size;
-	private int listHeight;
+	public int height;
 	private Node<T> head;
 
-	private static class Node<T>{
+	private static class Node<T> {
 		T item;
 		Node<T> forward;
 		Node<T> down;
 		int dist;
-		
-		Node(T item){
+
+		Node(T item) {
 			this.item = item;
 		}
-		
-		private void addNode() {
-			
-		}
 
+	}
+
+	/**
+	 * Creates an empty Skip List
+	 */
+	public SkipList() {
+		map = new TreeMap<>();
+		size = 0;
+		height = 0;
 	}
 
 	/**
 	 * Adds item to end of list
-	 * @param item		item to be added
-	 * @return true 	item has been added
+	 * 
+	 * @param item item to be added
+	 * @return true item has been added
 	 */
 	@Override
 	public boolean add(T item) {
 		// TODO Auto-generated method stub
+		map.put(size, item);
+		size++;
+		assignNodeHeight();// IDK what to put in as parameters
 		return true;
 	}
-	
+
 	/**
-	 * Adds item to specific index on list. Previous item in index gets moved forward
-	 * @param index		position in list item is added to.
-	 * @param item		item to be added
+	 * Adds item to specific index on list. Previous item in index gets moved
+	 * forward
+	 * 
+	 * @param index position in list item is added to.
+	 * @param item  item to be added
 	 */
 	@Override
 	public void add(int index, T item) {
 		// TODO Auto-generated method stub
 
 	}
-	
+
 	/**
 	 * Returns item inside specified index
-	 * @param index		position in the list the item to be returned has
-	 * @return item		item to be returned
+	 * 
+	 * @param index position in the list the item to be returned has
+	 * @return item item to be returned
 	 */
 	@Override
 	public T get(int index) {
@@ -60,16 +72,17 @@ public class SkipList<T> implements List<T> {
 		T item = null;
 		return item;
 	}
-	
+
 	/**
 	 * Returns size of list
-	 * @return size		size of list
+	 * 
+	 * @return size size of list
 	 */
 	@Override
 	public int size() {
 		return size;
 	}
-	
+
 	/**
 	 * Removes all items in list
 	 */
@@ -78,10 +91,11 @@ public class SkipList<T> implements List<T> {
 		// TODO Auto-generated method stub
 
 	}
-	
+
 	/**
 	 * Describes contents of list in string form
-	 * @return report		contents inside list
+	 * 
+	 * @return report contents inside list
 	 */
 	@Override
 	public String toString() {
@@ -90,13 +104,22 @@ public class SkipList<T> implements List<T> {
 		return report;
 
 	}
-	
-	private void assignNodeHeight() {
-		
+
+	private int assignNodeHeight() {
+		int i = 0;
+		boolean isHeightReached = false;
+		while (isHeightReached == false) {
+			if (Math.random() < 0.5) { // winning case
+				isHeightReached = true;
+			} else
+				i++;
+		}
+		return i;
 	}
-	
-	/* Below are any unused methods that aren't needed for the implementation,
-	 * but are necessary for using the List interface.
+
+	/*
+	 * Below are any unused methods that aren't needed for the implementation, but
+	 * are necessary for using the List interface.
 	 */
 
 	@Override
--- 2032assignment1/src/assignment1_test.java
+++ 2032assignment1/src/assignment1_test.java
@@ -5,8 +5,11 @@ import org.junit.Test;
 public class assignment1_test {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void addTester1() {
+		SkipList<String> list = new SkipList<>();
+		assertTrue(list.size() == 0 && list.height == 0);
+		list.add("1");
+		assertTrue(list.size() == 1 && list.height != 0);
 	}
 
 }
